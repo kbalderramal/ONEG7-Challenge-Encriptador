a052a0c3037a81a41cdd3e09cff436b45275a2b9
kbalderramal <172669667+kbalderramal@users.noreply.github.com>
2024-08-10 17:32:25 -0600

    feat: validation function for input message

--- app.js
+++ app.js
@@ -6,17 +6,28 @@ function getInput() {
     // obtener el valor ingreado en el textarea y asignarlo a la variable inputMessage
     inputMessage = document.getElementById("input-text").value;
     // medida provicional, imprimri el texto ingresado en el área de salida
-    document.getElementById("output-area").textContent = `${inputMessage}`; 
-    return inputMessage;   
+    // document.getElementById("output-area").textContent = `${inputMessage}`; 
+    // llamada a la función para valdiar el mensaje ingresado
+    inputValidation(inputMessage);   
+}
+
+function inputValidation(message) {
+    const regex = /^[a-z0-9\s]+$/;
+
+    if (regex.test(message)){
+        return True;
+    }else{
+        alert("Mensaje invalido\nIngrese solo letras minusculas o números.");
+    }
 }
 
 // TODO: 
 // [ x ] get input function
-// [ ] input validation function 
+// [ x ] input validation function 
+// [ ] clear input textarea 
 // [ ] encrypt text function
 // [ ] decrypt message function
 // [ ] copy button function
-// [ ] clear input textarea
 
 
 
