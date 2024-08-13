2cf5d3fb57a299f42bc90530154ef0959fe43e9d
kbalderramal <172669667+kbalderramal@users.noreply.github.com>
2024-08-13 12:21:03 -0600

    feat: text encryptor function & some minor adjustments in other functions' order of execution

--- app.js
+++ app.js
@@ -1,31 +1,64 @@
 let inputMessage;
 let encriptedText;
-let decryptMessage;
+let decryptText;
 
+// Obetener el mensaje ingresado en el textarea
 function getInput() {
-    // obtener el valor ingreado en el textarea y asignarlo a la variable inputMessage
+    // Guardar el texto ingresado a traves del id de la etiqueta
     inputMessage = document.getElementById("input-text").value;
-    // medida provicional, imprimri el texto ingresado en el área de salida
-    // document.getElementById("output-area").textContent = `${inputMessage}`; 
-    // llamada a la función para valdiar el mensaje ingresado
-    inputValidation(inputMessage);   
+    
+    // Verificar el texto
+    return inputValidation(inputMessage);
 }
 
+// Validar que el texto ingresado cumpla con los críterios establecidos en el reto
 function inputValidation(message) {
+    // Expresión rgular que acepta solo números y letras minuscuals
     const regex = /^[a-z0-9\s]+$/;
 
+    // Probar le expresión y retornar el valor booleano correspondiente
     if (regex.test(message)){
-        return True;
+        return true;
     }else{
-        alert("Mensaje invalido\nIngrese solo letras minusculas o números.");
+        // alert("Mensaje invalido\nIngrese solo letras minusculas o números.");
+        return false;
     }
+}
+
+// Encriptar texto según los parametrós establecidos en el reto
+function encrypt() {
+
+    // Permitir la ejecución del resto de la función solo si se cumple con lo establecido
+    if (!getInput()) {
+        alert("Mensaje inválido\nIngrese solo letras minúsculas o números.");
+        return;  // Detener la ejecución si el mensaje es inválido
+    }
+
+    encriptedText = inputMessage;
+
+    // Buscar y remplazar cada vocal por su respectivo nuevo valor
+    // /g indica que debe aplicarse el cambio para cada incidencia
+    encriptedText = encriptedText.replace(/e/g, "enter");
+    encriptedText = encriptedText.replace(/i/g, "imes");
+    encriptedText = encriptedText.replace(/a/g, "ai");
+    encriptedText = encriptedText.replace(/o/g, "ober");
+    encriptedText = encriptedText.replace(/u/g, "ufat");
+
+    // Imprimir en el textarea correspondiente el resultado
+    document.getElementById("output-area").textContent = `${encriptedText}`; 
+    return encriptedText;
+}
+
+// 
+function decrypt() {
+    // return decryptText;
 }
 
 // TODO: 
 // [ x ] get input function
 // [ x ] input validation function 
 // [ ] clear input textarea 
-// [ ] encrypt text function
+// [ x ] encrypt text function
 // [ ] decrypt message function
 // [ ] copy button function
 
